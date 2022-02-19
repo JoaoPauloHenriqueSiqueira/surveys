e26d56547233c2d06a7c4bee1f974b15590dca88
JoaoPauloHenriqueSiqueira <joao.henriquesiqueirati@gmail.com>
2022-02-19 14:06:11 -0300

    Logout implemented, navbar fixed, using vue-router

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -2,14 +2,25 @@ import { createStore } from "vuex"
 
 const store = createStore({    
     state:{
-        user:{
-            data:{name:"João"},
-            token:true
+        user : {
+        token:true,
+        data: {
+            name: 'Tom Cookss',
+            email: 'tom@example.com',
+            imageUrl:
+                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            }
         }
     },
+
     getters:{},
     actions:{},
-    mutations:{},
+    mutations:{
+        logout: (state) => {
+            state.user.data = {};
+            state.user.token = null;
+        }
+    },
     modules:{}
 })
 
