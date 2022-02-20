43e7090d48bc45d1f74f02b75943a6b2a02d5806
JoaoPauloHenriqueSiqueira <joao.henriquesiqueirati@gmail.com>
2022-02-20 20:28:01 -0300

    Migrations created, route and AuthController-register implemented. Vuex mutation register and token

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,27 +1,43 @@
 import { createStore } from "vuex"
 
-const store = createStore({    
-    state:{
-        user : {
-        token:true,
-        data: {
-            name: 'Tom Cookss',
-            email: 'tom@example.com',
-            imageUrl:
-                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            }
+const store = createStore({
+    state: {
+        user: {
+            token: sessionStorage.getItem("TOKEN"),
+            data: {}
         }
     },
 
-    getters:{},
-    actions:{},
-    mutations:{
+    getters: {},
+    actions: {
+        register({commit}, user){
+            return fetch(`http://localhost/laravel-vue-survey/public/api/register`,{
+                headers:{
+                    "Content-Type":"application/json",
+                    "Accept":"application/json"
+                },
+                method:"POST",
+                body:JSON.stringify(user)
+            })
+            .then((res) => res.json())
+            .then((res)=>{
+                commit("setUser",res);
+                return res;
+            });
+        }
+    },
+    mutations: {
         logout: (state) => {
             state.user.data = {};
             state.user.token = null;
+        },
+        setUser: (state, userData) => {
+            state.user.token = userData.token;
+            state.user.data = userData.user;
+            sessionStorage.setItem("TOKEN", userData.token);
         }
     },
-    modules:{}
+    modules: {}
 })
 
 export default store;
