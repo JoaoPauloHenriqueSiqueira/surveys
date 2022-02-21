169212ae008a5e04ef5440d070f85c2a9cffa80d
JoaoPauloHenriqueSiqueira <joao.henriquesiqueirati@gmail.com>
2022-02-21 16:00:50 -0300

    Axios added, Logout backend created. Login frontend implemented. Fecth removed and Axios being used

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,24 +35,38 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            'email' => 'required|email|string|unique:users,email',
+            'email' => 'required|email|string',
             'password' => [
-                'required'
-            ]
+                'required',
+            ],
+            'remember' => 'boolean'
         ]);
-
         $remember = $credentials['remember'] ?? false;
         unset($credentials['remember']);
-         
-        if(!Auth::attempt($credentials,$remember)){
+
+        if (!Auth::attempt($credentials, $remember)) {
             return response([
-                'error' => "The provided credentials are not correct"
+                'errors' => 'The Provided credentials are not correct'
             ], 422);
         }
-
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
-        
-        return response(['user' => $user, "token" => $token]);
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+        $user->currentAccessToken()->delete();
+
+        return response([
+            'success' => true
+        ]);
+    }
+
     }
 }
